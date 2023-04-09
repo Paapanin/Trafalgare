0b72fa5766c1f6a68e752bfb46f1744f4f3f3623
Paapanin <99012116+Paapanin@users.noreply.github.com>
2023-04-09 17:18:23 +0300

    Add files via upload

---
+++ Trafalgar/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    nav: true,
+    items: 1,
+    navText: ['<img src="../image/prev.svg">', '<img src="../image/next.svg">'],
+  });
+});
+
+$(document).ready(function () {
+  $(".menu-burger").click(function () {
+    $(".menu-burger").toggleClass("open-menu");
+    $(".navigation").toggleClass("open-menu");
+  });
+});
+
+$(window).scroll(function () {
+  var sb_m = 20; /* отступ сверху и снизу */
+  var mb = 300; /* высота подвала с запасом */
+  var st = $(window).scrollTop();
+  var sb = $(".sticky-block");
+  var sbi = $(".sticky-block .inner");
+  var sb_ot = sb.offset().top;
+  var sbi_ot = sbi.offset().top;
+  var sb_h = sb.height();
+
+  if (sb_h + $(document).scrollTop() + sb_m + mb < $(document).height()) {
+    if (st > sb_ot) {
+      var h = Math.round(st - sb_ot) + sb_m;
+      sb.css({ paddingTop: h });
+    } else {
+      sb.css({ paddingTop: 0 });
+    }
+  }
+});
